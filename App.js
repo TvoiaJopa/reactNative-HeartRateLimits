322f8eff34933f6343d4be3e68eeac15c0bb061d
Artemii Korshunov <pon4ickoff@yandex.ru>
2023-08-29 15:38:49 +0300

    init 2

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
 
 export default function App() {
+  const [number, onChangeNumber] = React.useState("");
+  const [max, onMax] = React.useState("");
+  const [min, onMin] = React.useState("");
+
+  const SetAge = (age) => {
+    onChangeNumber(age);
+    onMin((220 - age) * 0.65);
+    onMax((220 - age) * 0.85);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Age</Text>
+      <TextInput onChangeText={SetAge} style={styles.input} />
       <StatusBar style="auto" />
+      <Text>{min}</Text>
+      <Text>{max}</Text>
     </View>
   );
 }
@@ -13,8 +27,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
   },
 });
